51de9223e504bc32ca8b34538c5ba2a430d0094e
Oluwatito Ebiwonjumi <titoebiwonjumi@gmail.com>
2019-11-07 16:22:10 -0600

    Login Page start, please continue the rest on the login branch

---
+++ app/src/main/java/com/software/cleantheearthapp/LoginPageActivity.java
@@ -0,0 +1,13 @@
+package com.software.cleantheearthapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginPageActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
