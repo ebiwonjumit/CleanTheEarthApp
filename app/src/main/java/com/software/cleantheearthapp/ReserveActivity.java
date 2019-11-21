b20789d83d801a064bfae8aa681014d2a64b28a6
colevpark <colevpark@gmail.com>
2019-11-21 15:32:01 -0600

    reserve page

---
+++ app/src/main/java/com/software/cleantheearthapp/ReserveActivity.java
@@ -0,0 +1,14 @@
+package com.software.cleantheearthapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReserveActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reserve_page);
+    }
+}
